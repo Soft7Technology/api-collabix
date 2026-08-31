5e87f8881c1240f64cbcb5ed388a657962bbc262
SuhaniSharma43434343 <153818226+SuhaniSharma43434343@users.noreply.github.com>
2026-08-31 16:38:37 +0530

    solved build error

--- src/modules/auth/authController.ts
+++ src/modules/auth/authController.ts
@@ -1,15 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import crypto from "crypto";
 import Razorpay from "razorpay";
-import { AuthService, hashToken } from "../services/authService.js";
+import { AuthService, hashToken } from "../../services/authService.js";
 import {
   generateAccessToken,
   generateRefreshToken,
   comparePassword,
   hashPassword,
-} from "../utils/auth.js";
-import { db } from "../db/index.js";
-import { config } from "../config/index.js";
+} from "../../utils/auth.js";
+import { db } from "../../db/index.js";
+import { config } from "../../config/index.js";
 
 // Cookie configurations
 const cookieOptions = {
